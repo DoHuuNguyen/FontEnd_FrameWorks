8cd816c6a7916ee8262fd6e0bd61cdeed0057e98
DESKTOP-FQV8T5U\Acer <nguyendhph23086@fpt.edu.vn>
2023-01-13 00:39:56 +0700

    bài tập về nhà của thầy giao ne

---
+++ BaiTap/Buoi_12-1/baiTapVeNha/index.js
@@ -0,0 +1,35 @@
+//tao form sau :
+//hoten (Text)
+//gioitinh (select)
+//namsinh (text)
+//tao 1 nut kiem tra
+//khi an vao nut kiem tra hop thoai se hien len thong tin nhu sau:
+// Ong/Ba nguyen van a co/khong du dieu kien di nghia vu quan su
+//ong/ba phu thuoc vao gioi tinh minh chon
+//tuoi = namhientai - namsinh (get current year in javascript
+//tuoi >=18 va tuoi <=27 =>du dieu kien di nvqs
+// nguoc lai ko du dieu kien di nvqs
+function clickHere() {
+  let namsinh = document.getElementById("NamSinh").value;
+  var dateObj = new Date();
+  let year = dateObj.getFullYear();
+  let tuoi = year - namsinh;
+  let hovaten = document.getElementById("hovaten").value;
+  if (namsinh == "") {
+    swal("Xin lỗi", "Bạn chưa nhập Năm sinh", "error");
+  } else if (hovaten == "") {
+    swal("Xin lỗi", "Bạn chưa nhập họ và tên", "error");
+  } else {
+    if (tuoi >= 18 && tuoi <= 27) {
+      let gt = document.getElementById("gender").value;
+
+      if (gt === "0") {
+        swal("Chúc Mừng " + hovaten, "Đã đăng ký thành công", "success");
+      } else {
+        swal("Xin lỗi", "Bạn phải là nam", "error");
+      }
+    } else {
+      swal("Xin lỗi", "Bạn chưa đủ tuổi", "error");
+    }
+  }
+}
