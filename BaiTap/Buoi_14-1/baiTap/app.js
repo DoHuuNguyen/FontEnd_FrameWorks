39ad404675e01a874c6cee302fa4b217748c685b
DESKTOP-FQV8T5U\Acer <nguyendhph23086@fpt.edu.vn>
2023-01-31 14:36:00 +0700

    hom nay

--- BaiTap/Buoi_14-1/baiTap/app.js
+++ BaiTap/Buoi_14-1/baiTap/app.js
@@ -5,20 +5,10 @@ function myInfo($scope) {
   $scope.sdt = "01231231";
   $scope.nganh = "Ứng dụng phần mềm";
 }
+
+function sonDauBuoi($scope) {
+  $scope.ngu = "Chuc mung nam's moi";
+}
 var info = angular.module("a", []);
 info.controller("infome", myInfo);
-
-function yourInfo($scope) {
-  $scope.students = [
-    {
-      hovaten: "Van Dinh Son ",
-      age: "20",
-      sex: "Nam",
-      gmail: "sondt@gmail.com",
-      phone: "83742083",
-      chuyenNganh: "Ung dung phan mem",
-    },
-  ];
-  var your = angular.module("a", []);
-  your.controller("infome", yourInfo);
-}
+info.controller("sonDauBuoiController", sonDauBuoi);
