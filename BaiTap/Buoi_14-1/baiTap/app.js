29cf7f3192c120e76c954e72f250ebc1df4e9e24
DESKTOP-FQV8T5U\Acer <nguyendhph23086@fpt.edu.vn>
2023-01-31 14:49:33 +0700

    them object vao sonDauBuoi

--- BaiTap/Buoi_14-1/baiTap/app.js
+++ BaiTap/Buoi_14-1/baiTap/app.js
@@ -6,9 +6,20 @@ function myInfo($scope) {
   $scope.nganh = "Ứng dụng phần mềm";
 }
 
-function sonDauBuoi($scope) {
-  $scope.ngu = "Chuc mung nam's moi";
-}
+// function sonDauBuoi($scope) {
+//   $scope.ngu = "Chuc mung nam's moi";
+// }
 var info = angular.module("a", []);
 info.controller("infome", myInfo);
-info.controller("sonDauBuoiController", sonDauBuoi);
+// viet tat
+info.controller("sonDauBuoiController", function ($scope) {
+  $scope.ngu = "chuc mung nam's moi";
+  $scope.infoo = {
+    ten: "Son dau buoi",
+    tuoi: "18",
+    email: "sondaubuoi@gmail.com",
+    sdt: "01231231",
+    nganh: "NGu",
+  };
+});
+//
