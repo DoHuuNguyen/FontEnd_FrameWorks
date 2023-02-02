75a440bd43729542428da49418eaaeac43af1b6f
DESKTOP-FQV8T5U\Acer <nguyendhph23086@fpt.edu.vn>
2023-02-02 22:55:06 +0700

    lab 4 phan 2 bai 5 cuc dep

--- Lab/Lab4/Phan2/js.js
+++ Lab/Lab4/Phan2/js.js
@@ -5,3 +5,20 @@ b.controller("myCtrl", function ($scope) {
     $scope.ChuVi = ($scope.dai + $scope.rong) * 2;
   };
 });
+b.controller("myCtrlne", function ($scope) {
+  $scope.ten = "Nguyễn Văn Tèo";
+  $scope.ngaysinh = "31-1-2023";
+  $scope.gt = "nam";
+  $scope.diemtb = 5;
+  $scope.img = "img/teof.png";
+  var diem = parseFloat($scope.diemtb);
+  $scope.xeploai = function () {
+    if (diem >= 5) {
+      // $scope.danhgia = "Đậu";
+      swal("Chúc Mừng " + $scope.ten, "Đã Đậu", "success");
+    } else {
+      // $scope.danhgia = "Rớt";
+      swal("Xin lỗi", "Bạn Đã Rớt", "error");
+    }
+  };
+});
